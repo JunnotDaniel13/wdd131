d0b2e791a621ef20a92f7d6b0767dd293a14d000
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-05-20 20:43:40 +0300

    Add initial HTML structure, CSS styles, and JavaScript for dynamic date display

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const currentYearSpan = document.getElementById("currentyear");
+  if (currentYearSpan) {
+    currentYearSpan.textContent = new Date().getFullYear();
+  }
+
+  const lastModifiedParagraph = document.getElementById("lastModified");
+  if (lastModifiedParagraph) {
+    lastModifiedParagraph.textContent =
+      "Last Modified: " + document.lastModified;
+  }
+});

7fd874830baf9211828de457c907141018a82916
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-05-25 17:45:54 +0300

    Refactor button click event to create list items and delete buttons dynamically

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,41 @@ const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-const li = document.createElement("li");
-const deleteButton = document.createElement("button");
+button.addEventListener("click", function () {
+  if (input.value.trim() !== "") {
+    const li = document.createElement("li");
 
-li.textContent = input.value;
-deleteButton.textContent = "❌";
-li.append(deleteButton);
+    const deleteButton = document.createElement("button");
+    
+    li.textContent = input.value.trim(); 
+    
+    deleteButton.textContent = "❌";
+    
+    deleteButton.setAttribute(
+      "aria-label",
+      `Remove ${input.value.trim()}`
+    );
+    
+    li.append(deleteButton);
+    
+    list.append(li);
+    
+    deleteButton.addEventListener("click", function () {
+      list.removeChild(li); 
+      input.focus(); 
+    });
+    
+    input.value = "";
+  } else {
+    
+  }
+  input.focus();
+});
 
-list.append(li);
+
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault(); 
+    button.click(); 
+  }
+});
